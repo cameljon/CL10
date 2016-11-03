688fb6ade15f202fdd5acbf024f61bd98d234122
Jon Volden <volden.jon@gmail.com>
2016-11-03 18:58:55 -0500

    Update main.cpp

--- CL10/main.cpp
+++ CL10/main.cpp
@@ -25,3 +25,20 @@ int main()
 
 	return 0;
 }
+
+/*
+What is the advantage of using an array of Questions, rather than declaring each question as a separate variable?
+	You can ask the questions using an array. If you set the questions with another function you could do the same.
+
+This question type is essentially a "fill in the blank". How would you implement other types of questions, like True / False or Multiple Choice?
+	The answer could be a bool for true or false and you can use an array for multiple choices.
+
+For each type of question, what is in common? What is different?
+	The question will always be a string. The answers can be different types should always return true or false.
+	
+Based on what is common, can you figure out what would go in a QuestionBase class?
+
+Based on what is different, can you figure out what type of child classes of QuestionBase you would declare, what member variables they would have, and how the AskQuestion function work change?
+
+
+*/
